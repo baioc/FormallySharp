8de4b9fdce13a71b85b0726a1fc16a9294bf0ff7
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2021-08-07 16:29:33 -0300

    Add buttons

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -1,5 +1,6 @@
 module Index
 
+open System.Text.RegularExpressions
 open Elmish
 
 open Formally.Regular
@@ -15,36 +16,44 @@ type LexerSpec =
 
 type Project = { Id: string; Lexer: LexerSpec }
 
+let isValidId id = Regex.IsMatch(id, "\w+")
+
+module Regexp =
+    let tryParse s =
+        match s with
+        | "" -> None
+        | s -> Some <| Regexp.ofSeq s // TODO
+
 
 type Model =
     { ProjectInterface: Project
-      Error: string option }
+      RegularDefinition: string * string }
 
 type Msg =
-    | SetProjectId of string
+    | SetRegularDefinition of string * string
     | AddRegularDefinition of string * LexerPart
     | RemoveRegularDefinition of string
     | AddSeparator of Regexp
     | RemoveSeparator of Regexp
+    | SetProjectId of string
 
 let init () : Model * Cmd<Msg> =
     let emptyProject =
         { Id = ""
           Lexer =
-              { RegularDefinitions = Map.empty
+              { RegularDefinitions = Map.ofSeq [ ("zero", Token Regexp.Zero); ("one", Token Regexp.One) ]
                 Separators = Set.empty } }
 
     let model =
         { ProjectInterface = emptyProject
-          Error = None }
+          RegularDefinition = "", "" }
 
     model, Cmd.none
 
 let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
     match msg with
-    | SetProjectId id ->
-        { model with
-              ProjectInterface = { model.ProjectInterface with Id = id } },
+    | SetRegularDefinition (name, body) ->
+        { model with RegularDefinition = name, body },
         Cmd.none
 
     | AddRegularDefinition (id, def) ->
@@ -99,17 +108,159 @@ let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
                                   Separators = Set.remove regex separators } } },
         Cmd.none
 
+    | SetProjectId id ->
+        { model with
+              ProjectInterface = { model.ProjectInterface with Id = id } },
+        Cmd.none
+
 
 open Feliz
 open Feliz.Bulma
 
-let body model dispatch =
-    Bulma.title "test"
+let regularDefinitions (model: Model) (dispatch: Msg -> unit) =
+    let name, body = model.RegularDefinition
+
+    Bulma.block [
+        Bulma.content [
+            Html.ol [
+                for def in model.ProjectInterface.Lexer.RegularDefinitions do
+                    Html.li [ prop.text (string def) ]
+            ]
+        ]
+        Bulma.field.div [
+            field.isGrouped
+            prop.children [
+                Bulma.control.div [
+                    Bulma.input.text [
+                        prop.value name
+                        prop.placeholder "nome"
+                        prop.onChange (fun name -> SetRegularDefinition (name, body) |> dispatch)
+                    ]
+                ]
+                Bulma.control.div [
+                    control.isExpanded
+                    prop.children [
+                        Bulma.input.text [
+                            prop.value body
+                            prop.placeholder "expressão regular"
+                            prop.onChange (fun body -> SetRegularDefinition (name, body) |> dispatch)
+                        ]
+                    ]
+                ]
+                Bulma.control.div [
+                    Bulma.button.a [
+                        prop.text "adicionar"
+                        prop.disabled
+                            ((not <| isValidId name) || (Regexp.tryParse body |> Option.isNone))
+                        prop.onClick
+                            (fun _ -> // TODO
+                                AddRegularDefinition
+                                    (name,
+                                     Regexp.tryParse body
+                                     |> Option.get
+                                     |> Token)
+                                |> dispatch)
+                        color.isInfo
+                    ]
+                ]
+            ]
+        ]
+    ]
+
+let body (model: Model) (dispatch: Msg -> unit) =
+    Bulma.columns [
+        Bulma.column [
+            Bulma.card [
+                Bulma.cardHeader [
+                    Bulma.cardHeaderTitle.p [
+                        prop.text "Definições Regulares"
+                        cardHeaderTitle.isCentered
+                        size.isSize4
+                        color.hasBackgroundInfoDark
+                        color.hasTextInfoLight
+                    ]
+                ]
+                Bulma.cardContent [ regularDefinitions model dispatch ]
+            ]
+        ]
+        Bulma.column [
+            Bulma.columns [
+                Bulma.column [
+                    Bulma.card [
+                        Bulma.cardHeader [
+                            Bulma.cardHeaderTitle.p [
+                                prop.text "Entrada"
+                                cardHeaderTitle.isCentered
+                                size.isSize4
+                                color.hasBackgroundInfoDark
+                                color.hasTextInfoLight
+                            ]
+                        ]
+                        Bulma.cardContent [
+                            Bulma.textarea []
+                        ]
+                    ]
+                ]
+                Bulma.column [
+                    Bulma.card [
+                        Bulma.cardHeader [
+                            Bulma.cardHeaderTitle.p [
+                                prop.text "Tabela de Símbolos"
+                                cardHeaderTitle.isCentered
+                                size.isSize4
+                                color.hasBackgroundInfoDark
+                                color.hasTextInfoLight
+                            ]
+                        ]
+                        Bulma.cardContent [
+                            Bulma.table []
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+
+let toolbar (model: Model) (dispatch: Msg -> unit) =
+    let idInvalid = not (isValidId model.ProjectInterface.Id)
+
+    Bulma.level [
+        level.isMobile
+        prop.children [
+            Bulma.levelLeft []
+            Bulma.levelRight [
+                Bulma.levelItem [
+                    Bulma.text.p "projeto:"
+                    Bulma.input.text [
+                        prop.value model.ProjectInterface.Id
+                        prop.onTextChange (SetProjectId >> dispatch)
+                        prop.placeholder "identificador"
+                    ]
+                ]
+                Bulma.levelItem [
+                    Bulma.button.button [
+                        prop.text "abrir"
+                        prop.disabled idInvalid
+                        prop.onClick (fun _ -> SetProjectId "abrir" |> dispatch ) // TODO
+                        color.isDanger
+                    ]
+                ]
+                Bulma.levelItem [
+                    Bulma.button.button [
+                        prop.text "salvar"
+                        prop.disabled idInvalid
+                        prop.onClick (fun _ -> SetProjectId "salvar" |> dispatch ) // TODO
+                        color.isDanger
+                    ]
+                ]
+            ]
+        ]
+    ]
 
 let view (model: Model) (dispatch: Msg -> unit) =
     let repoUrl = "https://github.com/baioc/FormallySharp"
 
-    Html.body [
+    let header =
         Bulma.navbar [
             color.isPrimary
             prop.style [ style.padding (length.rem 1) ]
@@ -131,7 +282,8 @@ let view (model: Model) (dispatch: Msg -> unit) =
                 ]
             ]
         ]
-        Bulma.section [ body model dispatch ]
+
+    let footer =
         Bulma.footer [
             prop.style [
                 style.paddingTop (length.rem 1.5)
@@ -145,4 +297,30 @@ let view (model: Model) (dispatch: Msg -> unit) =
                 Html.p "© 2021 Gabriel B. Sant'Anna, Marcelo Contin, João Vitor"
             ]
         ]
+
+    let body =
+        Html.div [
+            color.hasBackgroundPrimaryLight
+            prop.children [
+                Bulma.section [
+                    prop.style [
+                        style.paddingTop (length.rem 1.0)
+                        style.paddingBottom (length.rem 1.0)
+                    ]
+                    prop.children [ toolbar model dispatch ]
+                ]
+                Bulma.section [
+                    prop.style [
+                        style.paddingTop (length.rem 1.0)
+                        style.paddingBottom (length.rem 2.0)
+                    ]
+                    prop.children [ body model dispatch ]
+                ]
+            ]
+        ]
+
+    Html.body [
+        header
+        body
+        footer
     ]
