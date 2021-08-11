916e5e0c722747fafe7a2c4bea5b403030984f2f
Marcelo Contin <marcelo.contin@bridge.ufsc.br>
2021-08-10 21:55:59 -0300

    Funciona cacete

--- tests/Shared/Converter.Tests.fs
+++ tests/Shared/Converter.Tests.fs
@@ -15,7 +15,37 @@ open Formally.Regular
 module Converter =
 
     let tests = testList "Converter" [
-        testCase "primeiro" <| fun _ ->
+        testCase "string ab gera regex ab" <| fun _ ->
             let string = "ab"
             Expect.equal (Converter.convertRegularDefinitionTextToRegexp(string)) (Regexp.ofSeq "ab") "should be equal"
+
+        testCase "a|b" <| fun _ ->
+            let string = "a|b"
+            let regexA = Regexp.singleton('a')
+            let regexB = Regexp.singleton('b')
+            let regex = regexA + regexB
+
+            Expect.equal (Converter.convertRegularDefinitionTextToRegexp(string)) (regex) "should be equal"
+
+        testCase "[A-Z]" <| fun _ ->
+            let string = "[A-Z]"
+            let regex = Regexp.ofSet(['A' .. 'Z'])
+            Expect.equal (Converter.convertRegularDefinitionTextToRegexp(string)) (regex) "should be equal"
+
+        testCase "[A-Za-z]" <| fun _ ->
+            let string = "[A-Za-z]"
+            let regexA = Regexp.ofSet(['A' .. 'Z'])
+            let regexB = Regexp.ofSet(['a' .. 'z'])
+            Expect.equal (Converter.convertRegularDefinitionTextToRegexp(string)) (regexA + regexB) "should be equal"
+
+        testCase "([0-9]|a)*" <| fun _ ->
+            let string = "([0-9]|a)*"
+            let regexA = Regexp.ofSet(['0' .. '9'])
+            let regexB = Regexp.singleton('a')
+            Expect.equal (Converter.convertRegularDefinitionTextToRegexp(string)) (!*(regexA + regexB)) "should be equal"
+
+        testCase "([0-9]|a)*([A-Za-z]abab|[0-9]zzzz)" <| fun _ ->
+            let string = "([0-9]|a)*([A-Za-z]abab|[0-9]zzzz)"
+            let regex = (!*(Regexp.ofSet(['0' .. '9']) + Regexp.singleton('a')) * (((Regexp.ofSet(['A' .. 'Z']) + Regexp.ofSet(['a' .. 'z'])) * Regexp.ofSeq "abab") + (Regexp.ofSet(['0' .. '9']) * Regexp.ofSeq "zzzz")))
+            Expect.equal (Converter.convertRegularDefinitionTextToRegexp(string)) (regex) "should be equal"
     ]
