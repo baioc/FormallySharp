cabace415780f264be8b4ddcb06716ba60ff0bec
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2021-09-13 17:24:03 -0300

    Improve CFG modeling

--- src/Shared/ContextFree.fs
+++ src/Shared/ContextFree.fs
@@ -1,24 +1,41 @@
-/// Grammar manipulation and pushdown automata for context free languages.
+/// Grammar manipulation and pushdown automata for context-free languages.
 
 namespace Formally.ContextFree
 
 
-type GrammarSymbol<'Symbol> =
-    | Terminal of 'Symbol
-    | NonTerminal of 'Symbol
+/// Disjoint union of grammar terminal and non-terminal symbols: `T + N`.
+type Symbol<'T, 'N> =
+    | Terminal of 'T
+    | NonTerminal of 'N
 
-/// Represents the (possibly empty) body of a production rule.
-type ContextFreeProduction<'S when 'S: comparison> = Set<GrammarSymbol<'S>>
+/// Represents the (possibly empty) body of a production rule: `(T + N)*`.
+type ProductionBody<'T, 'N> = Symbol<'T, 'N> list
 
-[<AutoOpen>]
-module ContextFreeProduction =
-    let inline (|Symbols|Epsilon|) (production: ContextFreeProduction<_>) =
-        if production = Set.empty then
-            Epsilon
-        else
-            Symbols production
+/// Models context-free production rules like `N -> (T + N)*`.
+type ContextFreeProduction<'T, 'N> = 'N * ProductionBody<'T, 'N>
 
-/// Defines a Context Free Grammar (CFG).
-type Grammar<'Symbol when 'Symbol: comparison> =
-    { Rules: Map<'Symbol, ContextFreeProduction<'Symbol>>
-      Initial: 'Symbol }
+/// Defines a Context-Free Grammar (CFG).
+type Grammar<'Terminal, 'NonTerminal when 'Terminal: comparison and 'NonTerminal: comparison> =
+    { Initial: 'NonTerminal
+      Rules: Set<ContextFreeProduction<'Terminal, 'NonTerminal>> }
+
+    member this.Terminals: Set<'Terminal> =
+        let terminals body =
+            body
+            |> Seq.choose
+                (function Terminal t -> Some t | NonTerminal n -> None)
+            |> Set.ofSeq
+        this.Rules
+        |> Seq.map (fun (head, body) -> terminals body)
+        |> Set.unionMany
+
+    member this.NonTerminals: Set<'NonTerminal> =
+        let nonTerminals body =
+            body
+            |> Seq.choose
+                (function Terminal t -> None | NonTerminal n -> Some n)
+            |> Set.ofSeq
+        this.Rules
+        |> Seq.map (fun (head, body) -> nonTerminals body |> Set.add head)
+        |> Set.unionMany
+        |> Set.add this.Initial
