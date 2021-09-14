38637c2a649634b5343b9f66b02d81fc203187c7
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2021-09-14 02:20:47 -0300

    Compute FIRST sets

--- src/Shared/ContextFree.fs
+++ src/Shared/ContextFree.fs
@@ -5,16 +5,16 @@ namespace Formally.ContextFree
 
 /// Disjoint union of grammar terminal and non-terminal symbols: `T + N`.
 type Symbol<'T, 'N> =
-    | Terminal of Option<'T>
+    | Terminal of 'T
     | NonTerminal of 'N
 
-/// Represents the (possibly empty) body of a production rule: `(T + N)*`.
+/// Represents the (possibly empty) body of a single production: `(T + N)*`.
 type ProductionBody<'T, 'N> = Symbol<'T, 'N> list
 
 /// Models context-free production rules like `N -> (T + N)*`.
 type ContextFreeProduction<'T, 'N> = 'N * ProductionBody<'T, 'N>
 
-/// Defines a Context-Free Grammar (CFG).
+/// Fully determines a Context-Free Grammar (CFG).
 type Grammar<'Terminal, 'NonTerminal when 'Terminal: comparison and 'NonTerminal: comparison> =
     { Initial: 'NonTerminal
       Rules: Set<ContextFreeProduction<'Terminal, 'NonTerminal>> }
@@ -42,38 +42,50 @@ type Grammar<'Terminal, 'NonTerminal when 'Terminal: comparison and 'NonTerminal
 
 [<RequireQualifiedAccess>]
 module Grammar =
-// não conseguimos pensar em como fazer isso na sintaxe dele, mas a gnt tentou deixar a ideia pra ver ctg Baiocchi
-    let rec first (symbol: Symbol<'T, 'N>) (grammar: Grammar<'T, 'N>) : Set<Option<'T>> =
-        let mutable nonTerminalRules = Set.empty
-        let mutable output = Set.empty
-        match symbol with 
-        | Terminal t -> Set.singleton t
-        | NonTerminal n -> 
-            nonTerminalRules <- Set.filter (fun (head, body) -> head = n) grammar.Rules
-            for rule in nonTerminalRules do
-                if (rule.body[0] = Terminal) then
-                    output <- output.Add rule.body[0]
-                elif (rule.body[0] = None) then
-                    output <- output.Add None
-                else
-                    output <- output + (first rule.body[0] grammar)
-            output
+    /// Finds the FIRST set of a given symbol sequence in a grammar.
+    let rec first (symbols: Symbol<'T, 'N> list) (grammar: Grammar<'T, 'N>) : Set<Option<'T>> =
+        match symbols with
+        // if we get an empty body, it means we are producing epsilon directly
+        // or every symbol in the sequence was nullable, so return { epsilon }
+        | [] -> Set.singleton None
+        // if the first symbol is a terminal T, then FIRST(T) is { T }
+        | Terminal t :: rest -> Set.singleton (Some t)
+        // if the symbol is a non-terminal, unite the FIRST of its productions
+        // (but with an altered grammar to avoid going infinite on cycles)
+        | NonTerminal n :: rest ->
+            let firstSet =
+                grammar.Rules
+                |> Seq.filter (fun (head, body) -> head = n)
+                |> Seq.map
+                    (fun (head, body) ->
+                        let grammar =
+                            { grammar with
+                                  Rules = Set.remove (head, body) grammar.Rules }
+                        first body grammar)
+                |> Set.unionMany
+            // if the resulting set is NOT nullable, we can stop here
+            if not (Set.contains None firstSet) then
+                firstSet
+            // otherwise, iterate down the rest of the body
+            else
+                Set.union
+                    (Set.remove None firstSet)
+                    (first rest grammar)
 
-        
-
-    let follow (symbol: Symbol<'T, 'N>) (grammar: Grammar<'T, 'N>): Set<Option<'T>> =
-        let mutable rulesWhereNonTerminalAppears = Set.empty
-        let mutable output = Set.empty
-        match symbol with 
-        | Terminal t -> Set.empty
-        | NonTerminal n -> 
-            if (n = grammar.Rules[0]) then
-                output <- output.Add(Set.empty.add(Terminal '$'))
-            rulesWhereNonTerminalAppears <- Set.filter (fun (head, body) -> body = n) grammar.Rules
-            for rule in rulesWhereNonTerminalAppears do
-                for production in rule do
-                    None
-            output
+    let follow (symbol: Symbol<'T, 'N>) (grammar: Grammar<'T, 'N>) : Set<Option<'T>> =
+        failwith "TODO: follow"
+        // let mutable rulesWhereNonTerminalAppears = Set.empty
+        // let mutable output = Set.empty
+        // match symbol with
+        // | Terminal t -> Set.empty
+        // | NonTerminal n ->
+        //     if (n = grammar.Rules[0]) then
+        //         output <- output.Add(Set.empty.add(Terminal '$'))
+        //     rulesWhereNonTerminalAppears <- Set.filter (fun (head, body) -> body = n) grammar.Rules
+        //     for rule in rulesWhereNonTerminalAppears do
+        //         for production in rule do
+        //             None
+        //     output
 
     let eliminateLeftRecursions (grammar: Grammar<'T, 'N>) : Grammar<'T, 'N> =
         failwith "TODO: eliminateLeftRecursions"
--- tests/Shared/ContextFree.Tests.fs
+++ tests/Shared/ContextFree.Tests.fs
@@ -9,11 +9,40 @@ open Expecto
 open Formally.ContextFree
 
 
-module ContextFree = 
+module ContextFree =
+    let (:=) head body = head, body
 
-    let tests = testList "First" [
-        testCase "primeiro" <| fun _ ->
-            let string = "Oi"
-            let teste = Set.add string Set.empty
-            Expect.equal (Grammar.first (Terminal "oi" ) { Initial = "inicial"; Rules = Set.empty } ) (teste) "deu erro"
+    let grammar =
+        { Initial = "S"
+          Rules = set [
+              // S -> aAa | bBb | C
+              "S" := [ Terminal 'a'; NonTerminal "A"; Terminal 'a' ]
+              "S" := [ Terminal 'b'; NonTerminal "B"; Terminal 'b' ]
+              "S" := [ NonTerminal "C" ]
+              // A -> Da | a
+              "A" := [ NonTerminal "D"; Terminal 'a' ]
+              "A" := [ Terminal 'a' ]
+              // B -> Db | b
+              "B" := [ NonTerminal "D"; Terminal 'b' ]
+              "B" := [ Terminal 'b' ]
+              // C -> CD | &
+              "C" := [ NonTerminal "C"; NonTerminal "D" ]
+              "C" := []
+              // D -> Aa | Bb | CD
+              "D" := [ NonTerminal "A"; Terminal 'a'; ]
+              "D" := [ NonTerminal "B"; Terminal 'b'; ]
+              "D" := [ NonTerminal "C"; NonTerminal "D" ]
+          ] }
+
+    let tests = testList "Grammars" [
+        testCase "First sets" <| fun _ ->
+            let testFirst symbols expected =
+                Expect.equal (Grammar.first symbols grammar) expected $"FIRST({symbols})"
+            testFirst [ Terminal 'a' ]    (set [ Some 'a' ])
+            testFirst [ Terminal 'b' ]    (set [ Some 'b' ])
+            testFirst [ NonTerminal "S" ] (set [ Some 'a'; Some 'b'; None ])
+            testFirst [ NonTerminal "A" ] (set [ Some 'a'; Some 'b' ])
+            testFirst [ NonTerminal "B" ] (set [ Some 'b'; Some 'a' ])
+            testFirst [ NonTerminal "C" ] (set [ None; Some 'a'; Some 'b' ])
+            testFirst [ NonTerminal "D" ] (set [ Some 'a'; Some 'b' ])
     ]
